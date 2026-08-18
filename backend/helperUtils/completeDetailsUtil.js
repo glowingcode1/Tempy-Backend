dc4a992abd5add55b2d7740b9f05b30cae32d9b1
glowingemp133 <glowingemp133@glowingsoft.com>
2026-08-18 17:38:25 +0500

    updateUser API

--- backend/helperUtils/completeDetailsUtil.js
+++ backend/helperUtils/completeDetailsUtil.js
@@ -15,7 +15,7 @@ const REQUIRED_FIELDS_BY_USER_TYPE = {
     "validationDocument",
   ],
   nurse: ["taxNumber", "governmentIdentity", "degree", "certification"],
-  user: ["name", "location"],
+  user: ["taxNumber", "governmentIdentity"],
   guest: [],
   admin: [],
   // TODO: add "employee" here once its discriminator schema exists (see Issue 2)
